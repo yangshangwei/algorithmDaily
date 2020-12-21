1c89e80da7601afc049c5d69849e4173e55fd371
yangshangwei <yswcomeon@gmail.com>
2020-12-21 09:24:28 +0800

    两数之和

---
+++ src/main/java/com/artisan/algorithm/daily2020/training12/T1221/TwoSum.java
@@ -0,0 +1,19 @@
+package com.artisan.algorithm.daily2020.training12.T1221;
+
+/**
+ * @author 小工匠
+ * @version 1.0
+ * @description: 给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ * @date 2020/12/21 9:16
+ * @mark: show me the code , change the world
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+
+
+}
+    
